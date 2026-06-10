a068e4f2096b1906fca4a4b59f64b0ea39cc9287
hyduongha <62180356+hyduongha@users.noreply.github.com>
2026-06-10 08:08:03 +0700

    Update Thuat_toan_K7.py

--- Thuat_toan_K7.py
+++ Thuat_toan_K7.py
@@ -1362,11 +1362,11 @@ def append_log_excel(
 
 def main(name):
     numbers = re.findall(r'\d+', name)
-    input_path = "/content/drive/MyDrive/Test/" + name
-    excel_path = os.path.join("/content/drive/MyDrive/Test/log"+numbers[0]+".xlsx")  # file Excel lưu
-    excel_path_Vector = os.path.join("/content/drive/MyDrive/Test/logVector"+numbers[0]+".xlsx")  # file Excel lưu
-    excel_path_Eig = os.path.join("/content/drive/MyDrive/Test/logEig"+numbers[0]+".xlsx")  # file Excel lưu
-    output_path = "/content/drive/MyDrive/Test/out"+numbers[0]
+    input_path = "/content/drive/MyDrive/Test1/" + name
+    excel_path = os.path.join("/content/drive/MyDrive/Test1/log"+numbers[0]+".xlsx")  # file Excel lưu
+    excel_path_Vector = os.path.join("/content/drive/MyDrive/Test1/logVector"+numbers[0]+".xlsx")  # file Excel lưu
+    excel_path_Eig = os.path.join("/content/drive/MyDrive/Test1/logEig"+numbers[0]+".xlsx")  # file Excel lưu
+    output_path = "/content/drive/MyDrive/Test1/out"+numbers[0]
 
     if not os.path.isdir(input_path):
         print(f"❌ Thư mục {input_path} không tồn tại!")
