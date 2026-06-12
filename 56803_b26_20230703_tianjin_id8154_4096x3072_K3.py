ee23e01f869a1910bc946659585319e91c801feb
hyduongha <62180356+hyduongha@users.noreply.github.com>
2026-06-12 09:26:34 +0700

    Update and rename 56803_b26_20230703_tianjin_id8154_4096x3072_K3 to 56803_b26_20230703_tianjin_id8154_4096x3072_K3.py

--- 56803_b26_20230703_tianjin_id8154_4096x3072_K3.py
+++ 56803_b26_20230703_tianjin_id8154_4096x3072_K3.py
@@ -1362,11 +1362,11 @@ def append_log_excel(
 
 def main(name):
     numbers = re.findall(r'\d+', name)
-    input_path = "/content/drive/MyDrive/Test/56803_b26_20230703_tianjin_id8154_4096x3072_K3/" + name
-    excel_path = os.path.join("/content/drive/MyDrive/Test/56803_b26_20230703_tianjin_id8154_4096x3072_K3/log"+numbers[0]+".xlsx")  # file Excel lưu
-    excel_path_Vector = os.path.join("/content/drive/MyDrive/Test/56803_b26_20230703_tianjin_id8154_4096x3072_K3/logVector"+numbers[0]+".xlsx")  # file Excel lưu
-    excel_path_Eig = os.path.join("/content/drive/MyDrive/Test/56803_b26_20230703_tianjin_id8154_4096x3072_K3/logEig"+numbers[0]+".xlsx")  # file Excel lưu
-    output_path = "/content/drive/MyDrive/Test/56803_b26_20230703_tianjin_id8154_4096x3072_K3/out"+numbers[0]
+    input_path = "/content/drive/MyDrive/56803_b26_20230703_tianjin_id8154_4096x3072_K3/" + name
+    excel_path = os.path.join("/content/drive/MyDrive/56803_b26_20230703_tianjin_id8154_4096x3072_K3/log"+numbers[0]+".xlsx")  # file Excel lưu
+    excel_path_Vector = os.path.join("/content/drive/MyDrive/56803_b26_20230703_tianjin_id8154_4096x3072_K3/logVector"+numbers[0]+".xlsx")  # file Excel lưu
+    excel_path_Eig = os.path.join("/content/drive/MyDrive/56803_b26_20230703_tianjin_id8154_4096x3072_K3/logEig"+numbers[0]+".xlsx")  # file Excel lưu
+    output_path = "/content/drive/MyDrive/56803_b26_20230703_tianjin_id8154_4096x3072_K3/out"+numbers[0]
 
     if not os.path.isdir(input_path):
         print(f"❌ Thư mục {input_path} không tồn tại!")
